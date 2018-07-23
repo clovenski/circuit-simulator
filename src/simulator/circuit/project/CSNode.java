7337c353cbf5fde7648c8f55d670767dc428bf6c
Joel Tengco <joeltengco@yahoo.com>
2018-07-22 23:44:17 -0700

    documentation for CSNode

--- src/simulator/circuit/project/CSNode.java
+++ src/simulator/circuit/project/CSNode.java
@@ -2,15 +2,50 @@ package simulator.circuit.project;
 
 import java.io.Serializable;
 
-// wraps the similarities of all the types of nodes in the graph, ie. DFlipFlop, AND gate, input variables, etc.
+/**
+ * Circuit Simulator node to represent entities within each circuit.
+ * <p>
+ * This is the super class to every single entity/node in a circuit.
+ * Each node will have a value, name, track number and type. The purpose of this class
+ * is to wrap all of the similarities between each entity within a graph such as a DFlipFlop,
+ * AND gate, input variable, etc.
+ * 
+ * @author Joel Tengco
+ */
 public abstract class CSNode implements Serializable {
-    protected int value;
+    /**
+     * The current value that this node has within the circuit; either 1 or 0.
+     */
+    protected int value;        // note: package needs access for building truth and transition tables in CSEngine
+    /**
+     * String to identify this node with.
+     */
     protected String name;
+    /**
+     * The node's track number within the program.
+     * <p>
+     * If this field is zero, this node is not being tracked by the program,
+     * otherwise a positive number indicates its track number.
+     */
     protected int trackNum;
-    // node type can be at most 6 characters, for formatting in menus
-    private final String NODE_TYPE;
+    /**
+     * String that is at most 6 characters to represent this node's type.
+     * <p>
+     * For example, input variables will have a node type of "INPUT".
+     */
+    private final String NODE_TYPE;         // node type can be at most 6 characters, for formatting in menus
+    /**
+     * Needed to implement Serializable, and thus needed for saving circuits as files
+     */
     private static final long serialVersionUID = 1L;
     
+    /**
+     * Constructs a new CSNode object with the given name and type.
+     * 
+     * @param name Identifier for this node.
+     * @param type Type of this node. If a string longer than 6 characters is given,
+     * then only the first 6 characters are used.
+     */
     public CSNode(String name, String type) {
         value = 0;
         this.name = name;
@@ -21,34 +56,73 @@ public abstract class CSNode implements Serializable {
         trackNum = 0;
     }
 
+    /**
+     * Sets a new name for this node.
+     * 
+     * @param newName String representing the new name for this node.
+     */
     public void setName(String newName) {
         name = newName;
     }
 
+    /**
+     * Gets this node's name/identifier.
+     * 
+     * @return The current name of this node.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets this node's current value.
+     * 
+     * @return Either 1 or 0 depending on this node's status in the circuit.
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * Resets this node's value back to zero.
+     */
     public void resetValue() {
         value = 0;
     }
 
+    /**
+     * Sets this node's track number.
+     * 
+     * @param trackNum Positive integer representing this node's new track number.
+     */
     public void setTrackNum(int trackNum) {
-        this.trackNum = trackNum;
+        if(trackNum > 0)
+            this.trackNum = trackNum;
     }
 
+    /**
+     * Gets this node's track number.
+     * 
+     * @return Integer greater than or equal to zero; representing the track number.
+     */
     public int getTrackNum() {
         return trackNum;
     }
 
+    /**
+     * Resets this node's track number back to zero, indicating this node is not
+     * being tracked.
+     */
     public void resetTrackNum() {
         trackNum = 0;
     }
 
+    /**
+     * Gets this node's type.
+     * 
+     * @return A string that is at most 6 characters long, representing this node's
+     * type.
+     */
     public String getNodeType() {
         return NODE_TYPE;
     }
@@ -67,5 +141,11 @@ public abstract class CSNode implements Serializable {
         return NODE_TYPE.hashCode() + name.hashCode() + trackNum;
     }
 
+    /**
+     * Update this node's value in the circuit.
+     * <p>
+     * In other words, this node's value field will be updated in a way that
+     * corresponds to how this node functions within a working circuit.
+     */
     public abstract void updateValue();
 }
